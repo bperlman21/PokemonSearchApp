672b472ab40f4428e752c5ae2676c0f4c3e68c18
bperlman21 <brad18@bellsouth.net>
2024-11-09 20:45:43 -0500

    updated JS to fetch pokemon API and fill out info and stats appropriately

--- index.js
+++ index.js
@@ -1,2 +1,72 @@
-console.log('Happy developing ✨');
-console.log('test');
+const searchInput = document.getElementById("search-input");
+const searchBtn = document.getElementById("search-button");
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const pokemonWeight = document.getElementById("weight");
+const pokemonHeight = document.getElementById("height");
+const pokemonType = document.getElementById("types");
+const pokemonSprite = document.getElementById('pokemon-sprite');
+const hp = document.getElementById("hp");
+const attack = document.getElementById("attack");
+const defense = document.getElementById("defense");
+const specialAttack = document.getElementById("special-attack");
+const specialDefense = document.getElementById("special-defense");
+const speed = document.getElementById("speed");
+
+async function getData() {
+    const url = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${searchInput.value.toLowerCase()}`;
+
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        console.log(json);
+
+        const name = json.name;
+        pokemonName.innerHTML = name;
+
+        const id = json.id;
+        pokemonId.innerHTML = `#${id}`;
+
+        const weight = json.weight;
+        pokemonWeight.innerHTML = `Weight: ${weight}`;
+
+        const height = json.height;
+        pokemonHeight.innerHTML = `Height: ${height}`;
+
+        const type = json.types[0].type.name;
+        pokemonType.innerHTML = type;
+
+        const sprite = json.sprites.front_default;
+        pokemonSprite.src = sprite;
+        pokemonSprite.style.display = "block";
+
+        const pokemonHp = json.stats[0].base_stat;
+        hp.innerHTML = pokemonHp;
+
+        const pokemonAttack = json.stats[1].base_stat;
+        attack.innerHTML = pokemonAttack;
+
+        const pokemonDefense = json.stats[2].base_stat;
+        defense.innerHTML = pokemonDefense;
+
+        const pokemonSpecialAttack = json.stats[3].base_stat;
+        specialAttack.innerHTML = pokemonSpecialAttack;
+
+        const pokemonSpecialDefense = json.stats[4].base_stat;
+        specialDefense.innerHTML = pokemonSpecialDefense;
+
+        const pokemonSpeed = json.stats[5].base_stat;
+        speed.innerHTML = pokemonSpeed;
+
+
+    } catch (error) {
+        console.error(error.message);
+    }
+
+}
+
+searchBtn.addEventListener("click", getData);
